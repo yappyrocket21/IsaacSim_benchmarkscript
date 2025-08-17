74fd9a8f0bbb3c300b191f3ba476dd6ca6e931e9
yappyrocket21 <90362710+yappyrocket21@users.noreply.github.com>
2025-08-17 00:45:53 -0700

    Starting work on extendable metric collection and organization

--- existingoutput.py
+++ existingoutput.py
@@ -2,13 +2,16 @@
 #Date: 2023-10-04
 #Purpose: Given folder of benchmark metrics from either 100x100 or 1080p IsaacSim rendering, generate CSV files measuring Mean FPS, GPU Dedicated Memory after runtime, Mean App Update Frametime, and System Memory RSS
 #Hypothesis: Linear relationship as camera count increases.
+#Observations: FPS decreases exponentially with camera count increase, everything else has linear relationship
 from pathlib import Path
 import re
 try:
     folder_path = Path(input("Enter the folder path: "))
     csv_path = Path(input("Enter the output CSV file path (without csv file extension): ")+".csv")
     with csv_path.open("w") as csvfile:
-        csvfile.write("Camera Count,Mean FPS,Mean App Update Frametime,GPU Memory Dedicated,System Memory RSS\n")
+        headCol1 = ["Camera Count", "GPU Memory Dedicated", "Mean FPS", "Mean App Update Frametime", "System Memory RSS"]
+        headCol2 = ["Camera Count", "GPU Memory Dedicated", "Mean FPS", "Mean App Update Frametime", "System Memory RSS"]
+        csvfile.write(",".join(headCol1) + "\n")
         for file_path in folder_path.iterdir():
             gpuDedMemFlag=0
             with file_path.open("r") as file:
@@ -30,7 +33,7 @@ try:
                         meanApp = line.split("Mean App_Update Frametime:")[1].strip().split("ms")[0].strip()
                     if "System Memory RSS:" in line:
                         sysMem = line.split("System Memory RSS:")[1].strip().split("GB")[0].strip()
-            csvfile.write(f"{cameraCount},{meanFPS},{meanApp},{gpuMem},{sysMem}\n")
+            csvfile.write(f"{cameraCount},{gpuMem},{meanFPS},{meanApp},{sysMem}\n")
 except Exception as e:
     print(f"An error occurred: {e}")
     exit(1)
